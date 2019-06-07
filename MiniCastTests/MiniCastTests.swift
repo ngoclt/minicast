7049b9994a7674c5ad912fddfc25b8e8e4c4213b
Nick Le <ngoc.le@maas.global>
2019-06-07 15:47:02 +0300

    Created project with simple web view screen

---
+++ MiniCastTests/MiniCastTests.swift
@@ -0,0 +1,34 @@
+//
+//  MiniCastTests.swift
+//  MiniCastTests
+//
+//  Created by Ngoc Le on 07/06/2019.
+//  Copyright © 2019 Coder Life. All rights reserved.
+//
+
+import XCTest
+@testable import MiniCast
+
+class MiniCastTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MiniCast' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MiniCast
+  pod 'XCGLogger', '~> 7.0.0'
+  pod 'Material', '~> 3.1.0'
+
+  target 'MiniCastTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MiniCastUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
